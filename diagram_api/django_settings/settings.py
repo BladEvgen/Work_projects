38b0eccf3b5f07ec5b6e8e294b07de974be4506f
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-02-24 11:52:35 +0600

    some update

--- diagram_api/django_settings/settings.py
+++ diagram_api/django_settings/settings.py
@@ -28,13 +28,12 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://localhost:8000",
-    "http://127.0.0.1:9000",
-    "http://127.0.0.1:3000",
+    "http://localhost:5000",
     "http://127.0.0.1:5000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "http://127.0.0.1",
-    "https://commander.medkrmu.kz",
+    "http://localhost",
 ]
 
 # ВЫКЛЮЧИТЬ МЕНЯ ПОТОМ
