41a883373cccd9ba22426ce58ac2b591e2e7e274
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-02-24 11:53:28 +0600

    uodate

--- diagram_api/django_app/config.py
+++ diagram_api/django_app/config.py
@@ -1,16 +1,34 @@
+import os
+
 import mysql.connector
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 
-def get_database_connection():
+def platonus_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="5bc!oMJOh-vytc@p",
-        database="platonus",
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
     )
 
 
-def execute_query(connection, query):
+def journal_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME_2"),
+    )
+
+
+def execute_query(connection, query, many=False):
     with connection.cursor() as cursor:
         cursor.execute(query)
+        if many:
+            return cursor.fetchall()
         return cursor.fetchone()[0]
--- diagram_api/django_app/urls.py
+++ diagram_api/django_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("diagram/", views.specialty_api, name="speciality_api"),
+    path("get_marks/", views.get_marks_for_tutor, name="get_marks"),
     path("load_data/", views.load_data, name="load_data"),
+    path("charts", views.charts, name="charts"),
     path("", views.home, name="home"),
 ]
