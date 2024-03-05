34a401ac875a8afd52f0ea4602d1a09210728ca0
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-03-05 16:20:18 +0500

    update

--- diagram_api/django_settings/urls.py
+++ diagram_api/django_settings/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("/", include("django_app.urls")),
+    path("api/", include("django_app.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
