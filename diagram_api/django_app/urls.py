4dfce56d57489070356c85d7edcf2142dcdd1291
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-03-05 18:02:02 +0500

    fix url problems

--- diagram_api/django_app/urls.py
+++ diagram_api/django_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path("diagram/", views.specialty_api, name="speciality_api"),
-    path("get_marks/", views.get_marks_for_tutor, name="get_marks"),
+    path("api/diagram/", views.specialty_api, name="speciality_api"),
+    path("api/get_marks/", views.get_marks_for_tutor, name="get_marks"),
     path("load_data/", views.load_data, name="load_data"),
-    path("charts", views.charts, name="charts"),
-    path("second_charts/", views.tutor_charts, name="tutor_charts"),
     path("charts/", views.charts, name="charts"),
+    path("second_charts/", views.tutor_charts, name="tutor_charts"),
     path("charts_2/", views.charts_second, name="charts2"),
     path("", views.home, name="home"),
 ]
