7f59fa05cdd95abcdee600f51c60671e2ab881ea
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-03-20 14:15:00 +0500

    ignore some ip addresses

--- diagram_api/django_app/middleware.py
+++ diagram_api/django_app/middleware.py
@@ -8,26 +8,23 @@ class LogAccessMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        ignored_ips = ["192.168.12.27", "192.168.11.130"]
+        ip_address = request.META.get("REMOTE_ADDR")
+        if ip_address in ignored_ips:
+            return self.get_response(request)
+
         if request.path.startswith("/admin/"):
             return self.get_response(request)
 
         response = self.get_response(request)
 
-        ip_address = request.META.get("REMOTE_ADDR")
-        if not ip_address:
-            ip_address = request.META.get("HTTP_X_FORWARDED_FOR", "")
-
         user_agent = request.META.get("HTTP_USER_AGENT", "")
         parsed_agent = httpagentparser.detect(user_agent)
 
         device = parsed_agent.get("platform", "Unknown")
-
         os = parsed_agent.get("os", {}).get("name", "Unknown")
-
         browser = parsed_agent.get("browser", {}).get("name", "Unknown")
-
         route = request.path
-
         access_time = timezone.now()
 
         models.AccessLog.objects.create(
