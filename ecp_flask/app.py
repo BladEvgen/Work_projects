3e682655a70e53f10e3be2e26a0d7f5ba5753dfc
bladevgen <ekozlov00@mail.ru>
2024-03-30 00:13:15 +0500

    db + template to check sign

--- ecp_flask/app.py
+++ ecp_flask/app.py
@@ -2,44 +2,75 @@ import os
 import json
 import base64
 import requests
+import datetime
+import mysql.connector
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+from uuid import uuid4
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 CORS(app)
 
+db_connection = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+)
+db_cursor = db_connection.cursor()
+
+db_cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS signed_files (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        original_file_name VARCHAR(255),
+        signed_file_path VARCHAR(255),
+        sign_time DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+"""
+)
+
+# Установка папки для загрузок
+app.config["UPLOAD_FOLDER"] = os.path.join(app.root_path, "static", "uploads")
+
+
+def save_to_database(original_file_name, signed_file_path):
+    sql = "INSERT INTO signed_files (original_file_name, signed_file_path) VALUES (%s, %s)"
+    val = (original_file_name, signed_file_path)
+    db_cursor.execute(sql, val)
+    db_connection.commit()
+
 
 @app.route("/sign", methods=["POST"])
 def sign_file():
-    app.config["UPLOAD_FOLDER"] = ""  # Папка, куда сохранять загруженные файлы
     key_files = request.files.getlist("keyPath")
     files = request.files.getlist("file")
     password = request.form["password"]
 
-    # Обработка загруженных файлов
     for file in key_files:
-        # Сохраняем файлы в нужном месте или обрабатываем их
         file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
         key_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
     for file in files:
-        # Сохраняем файлы в нужном месте или обрабатываем их
         file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
         file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
 
     returnfile = sign_file_gos(key_path, password, file_path)
 
     response = make_response(returnfile)
-    response.headers[
-        "Content-Disposition"
-    ] = 'attachment; filename="your_filename.extension"'
-
+    response.headers["Content-Disposition"] = 'attachment; filename="{}"'.format(
+        os.path.basename(file_path)
+    )
     return response
 
 
 def sign_file_gos(key, password, file):
     base64_key_string = get_base_64_key_string(key)
-    print(f"file --------------------------- {file}")
-    with open(file, "rb") as files:  # Открываем файловый поток из объекта FileStorage
+    with open(file, "rb") as files:
         file_contents = files.read()
     encoded_key = base64.b64encode(file_contents)
     file_contents = encoded_key.decode("utf-8")
@@ -60,45 +91,54 @@ def sign_file_gos(key, password, file):
 
     decoded_data = base64.b64decode(encoded_data)
 
-    with open("decoded_file.cms", "wb") as f:
+    unique_id = str(uuid4())
+    unique_filename = "decoded_file_" + str(unique_id[:10]) + ".cms"
+    decoded_file_path = os.path.join(app.config["UPLOAD_FOLDER"], unique_filename)
+
+    with open(decoded_file_path, "wb") as f:
         f.write(decoded_data)
+
+    save_to_database(os.path.basename(file), decoded_file_path)
+
     if os.path.exists(key):
         os.remove(key)
     if os.path.exists(file):
         os.remove(file)
+
     return decoded_data
 
 
 def get_base_64_key_string(key) -> str:
-    with open(key, "rb") as file:  # Открываем файловый поток из объекта FileStorage
+    with open(key, "rb") as file:
         key_contents = file.read()
     encoded_key = base64.b64encode(key_contents)
     base64_key_string = encoded_key.decode("utf-8")
-
     return base64_key_string
 
 
-@app.route("/verify", methods=["POST", "GET"])
+@app.route("/verify", methods=["GET"])
 def verify_data():
-    revocation_check = ["OCSP"]
+    file_id = request.args.get("id")
+    if not file_id:
+        return "File ID is required", 400
 
-    file_path = "/var/www/ecp.medkrmu/KRMU-main/decoded_file.cms"
+    db_cursor.execute(
+        "SELECT signed_file_path FROM signed_files WHERE id = %s", (file_id,)
+    )
+    result = db_cursor.fetchone()
+    if not result:
+        return "File not found", 404
+
+    file_path = result[0]
 
     with open(file_path, "rb") as f:
         data_to_verify = base64.b64encode(f.read()).decode("utf-8")
 
     url = "http://localhost:14579/cms/verify"
-
     headers = {"Content-Type": "application/json"}
-
-    payload = {"revocationCheck": revocation_check, "cms": data_to_verify}
-
-    payload_json = json.dumps(payload)
-
-    response = requests.post(url, data=payload_json, headers=headers)
-
+    payload = {"revocationCheck": ["OCSP"], "cms": data_to_verify}
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
     response_data = response.json()
-
     return jsonify(response_data)
 
 
