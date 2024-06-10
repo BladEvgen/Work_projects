54263a442020c7511dba3f5958c822d391a17c1f
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-06-10 11:54:14 +0300

    update

--- ochered/django_settings/asgi.py
+++ ochered/django_settings/asgi.py
@@ -1,14 +1,13 @@
 import os
 import django
-
-
-import ochered_app.routing
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-django.setup()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_settings.settings")
+django.setup()
+
+import ochered_app.routing
 
 application = ProtocolTypeRouter(
     {
