ca878daaeb6f9d36fa3187c30b3e6dfe8c1dc496
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2024-06-12 16:14:56 +0500

    logic to set timeout status

---
+++ ochered/ochered_app/management/commands/check_tickets.py
@@ -0,0 +1,23 @@
+from django.utils import timezone
+from ochered_app.models import Ticket
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Checks tickets and updates their status to timeout if they are older than 120 minutes"
+
+    def handle(self, *args, **kwargs):
+        timeout_threshold = timezone.now() - timezone.timedelta(minutes=120)
+        tickets_to_timeout = Ticket.objects.filter(
+            created_at__lt=timeout_threshold, status="waiting"
+        )
+
+        for ticket in tickets_to_timeout:
+            ticket.status = "timeout"
+            ticket.save()
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully updated {tickets_to_timeout.count()} tickets to timeout"
+            )
+        )
