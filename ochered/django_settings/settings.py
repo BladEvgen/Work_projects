bfb9b40b3216a2b1a1397f60353f2c6d31fe0763
bladevgen <ekozlov00@mail.ru>
2024-09-06 11:09:10 +0500

    Logger with Domain added

--- ochered/django_settings/settings.py
+++ ochered/django_settings/settings.py
@@ -1,5 +1,6 @@
 import os
 import socket
+from datetime import datetime
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -20,7 +21,16 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 МБ
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 1000000
 
-ALLOWED_HOSTS = ["*"]
+if DEBUG:
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = ["queue.krmu.edu.kz", "dot.medkrmu.kz", "localhost", "127.0.0.1"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://queue.krmu.edu.kz",
+    "https://dot.medkrmu.kz",
+    "http://localhost:8000",
+]
 
 INSTALLED_APPS = [
     # Installed apps
@@ -40,7 +50,20 @@ INSTALLED_APPS = [
     "ochered_app",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:5002",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:8000",
+    "http://localhost:5002",
+    "https://dot.medkrmu.kz",
+    "https://queue.krmu.edu.kz",
+]
 
 MIDDLEWARE = [
     "ochered_app.middleware.LogAccessMiddleware",
@@ -87,13 +110,24 @@ CHANNEL_LAYERS = {
     },
 }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.getenv("db_name"),
+            "USER": os.getenv("db_user"),
+            "PASSWORD": os.getenv("db_password"),
+            "HOST": os.getenv("db_host"),
+            "PORT": os.getenv("db_port"),
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -128,3 +162,41 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "static/media"
+
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {message}",
+            "style": "{",
+            "datefmt": "%Y-%m-%d %H:%M",
+        },
+    },
+    "handlers": {
+        "file": {
+            "level": "INFO" if DEBUG else "WARNING",
+            "class": "logging.FileHandler",
+            "filename": os.path.join(
+                LOG_DIR, f'log-{datetime.now().strftime("%Y-%m-%d_%H")}.log'
+            ),
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["file"],
+            "level": "INFO" if DEBUG else "WARNING",
+            "propagate": False,
+        },
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO" if DEBUG else "WARNING",
+            "propagate": False,
+        },
+    },
+}
