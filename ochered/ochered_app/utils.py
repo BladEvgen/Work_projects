f01dee9f1b04126816e911016e1cbb1d7950a0d3
bladevgen <ekozlov00@mail.ru>
2024-09-06 11:09:47 +0500

    Bigger QR generation

--- ochered/ochered_app/utils.py
+++ ochered/ochered_app/utils.py
@@ -3,13 +3,14 @@ import re
 import qrcode
 from django.conf import settings
 
+
 def generate_qr_code():
     server_domain_ip = settings.SERVER_DOMAIN_IP
 
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
+        box_size=20,
         border=4,
     )
 
@@ -17,6 +18,12 @@ def generate_qr_code():
     qr.make(fit=True)
 
     img = qr.make_image(fill_color="black", back_color="white")
+
+    min_size = 500
+    if img.size[0] < min_size or img.size[1] < min_size:
+        img = img.resize(
+            (max(min_size, img.size[0]), max(min_size, img.size[1])), resample=0
+        )
 
     media_path = settings.MEDIA_ROOT
     qr_code_path = os.path.join(media_path, "qr-code.png")
